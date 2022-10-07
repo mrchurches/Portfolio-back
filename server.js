787ba6becf62a30241f4c58788b7bb5918b7adba
mrchurches <laureanoiglesias34@gmail.com>
2022-10-07 00:48:09 -0300

    cors

--- server.js
+++ server.js
@@ -7,12 +7,18 @@ const {EMAIL_USER, EMAIL_PASS,CORS_URL} = process.env;
 // server used to send emails
 const app = express();
 
-const corsOptions = {
-	origin: CORS_URL,
-	optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
+// const corsOptions = {
+// 	origin: CORS_URL,
+// 	optionsSuccessStatus: 200
+// };
+//app.use(cors(corsOptions));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', CORS_URL); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'false');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST');
+  next();
+});
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running on 5000"));
