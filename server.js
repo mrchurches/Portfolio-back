d2cfb42e3737b1981c44f355d9dba44ed257efdf
mrchurches <laureanoiglesias34@gmail.com>
2022-10-07 00:49:39 -0300

    server fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // 	optionsSuccessStatus: 200
 // };
 //app.use(cors(corsOptions));
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', CORS_URL); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'false');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
